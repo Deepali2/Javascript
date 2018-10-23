3145af730b4c8bd3fce7c15445f2e8eac9732053
Deepali2 <hdgarg@gmail.com>
2018-10-22 17:33:51 -0700

    creates a project in which the user's age is days is returned

---
+++ Stalker JavaScript/script.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Hi! What is your first name?");
+let lastName = prompt("What is your last name?");
+let age = prompt("Now tell me your age.");
+console.log(`Your name is ${firstName} ${lastName}. You are ${age} years old. You are my friend`);
+alert(`${firstName} ${lastName} age ${age}. Will you be my friend? If yes, then check the console. I have printed your name and age on it!!!`);
+let days = age * 365.25;
+alert(`Hey my friend. I have been learning how to multiply. Since you told me that you are ${age} years old, I can tell you that you are also roughly ${days} days old.`)
